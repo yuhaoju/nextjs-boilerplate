855ff98c3105e16b1320061d648fd56db9cda0f2
Yuhao Ju <yuhao@tubi.tv>
2025-02-19 16:20:51 +0800

    feat: return ua info

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,5 +1,9 @@
 export const runtime = "edge";
 
-export async function GET() {
-  return new Response(`Hello from Edge!`);
+export async function GET(request: Request) {
+  // Get device and user info from request
+  const userAgent = request.headers.get("user-agent");
+
+  // Return user agent info as string
+  return new Response(`Hello from Edge! ${userAgent}`);
 }
